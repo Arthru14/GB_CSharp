02fe29015c2d6941bb8029d5f7b7d2608b93175c
Arthur Shmakov <shmakov.arthur@yandex.ru>
2023-10-09 21:31:10 +0500

    ДЗ 6 задача 41

---
+++ dz06_41/Program.cs
@@ -0,0 +1,16 @@
+﻿bool checkPositiveNums(int num = 0){
+    if (num > 0) {return true;}
+    return false;
+}
+
+Console.WriteLine("Введите количество чисел:");
+int M = Convert.ToInt32(Console.ReadLine());
+int numOfPositive = 0;
+
+Console.WriteLine("Введите числа для проверки на позитивность:");
+
+for (int i = 0; i < M; i++) {
+    int numFromUser = Convert.ToInt32(Console.ReadLine());
+    numOfPositive += Convert.ToInt32(checkPositiveNums(numFromUser));
+}
+Console.WriteLine($"Вы ввели {numOfPositive} положительных чисел");
